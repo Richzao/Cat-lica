64b506a7462afc141c3bf311857abced1487cc93
Ricardo Jr <ricardo.mouradf4@gmail.com>
2025-02-28 10:44:49 -0300

    Alteração do nome da pasta

--- programacao-web/atividade1/variaveisOperadores.js
+++ programacao-web/atividade1/variaveisOperadores.js
@@ -1,6 +1,6 @@
 /*
     Faça a declaração de 2 variáveis numéricas para cada operador e guarde o resultado da operação em uma terceira variável;
-    Para cada resultado obtido, imprima o texto “Resultado da {operação} entre {num_A} e {num_B} é {resultado}
+    Para cada resultado obtido, imprima o texto Resultado da {operação} entre {num_A} e {num_B} é {resultado}
 */
 
 let xAdd = 3;
